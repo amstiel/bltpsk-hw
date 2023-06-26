5fb9241edb1b07fa469c3a8887ce09c74cb32541
Alexey Kunitsky <knt@feip.co>
2023-06-26 22:53:26 +0200

    Add amount block to cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,10 @@
 'use client';
 
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
+
+import { selectTotalProductsAmount } from '@/redux/features/cart/selector';
+import { StoreState } from '@/redux/store';
 
 import { Container } from '@/components/Container/Container';
 import { MoviesList } from '@/components/MoviesList/MoviesList';
@@ -8,10 +12,21 @@ import { MoviesList } from '@/components/MoviesList/MoviesList';
 import styles from './page.module.scss';
 
 const CartPage: FC = () => {
+  const totalAmount = useSelector<StoreState, number>((state) => selectTotalProductsAmount(state));
+
   return (
-    <main className={styles.content}>
+    <main className={styles.root}>
       <Container>
-        <MoviesList mode="cart" />
+        <div className={styles.content}>
+          <MoviesList mode="cart" />
+
+          {totalAmount > 0 && (
+            <div className={styles.summaryBlock}>
+              <h5>Итого билетов</h5>
+              {totalAmount}
+            </div>
+          )}
+        </div>
       </Container>
     </main>
   );
