5c5f0ef9a4c15b6a8e905665991c144212483321
Alexey Kunitsky <knt@feip.co>
2023-06-26 15:47:43 +0200

    Persist cart state

--- src/redux/StoreProvider.tsx
+++ src/redux/StoreProvider.tsx
@@ -1,9 +1,17 @@
 'use client';
 
-import { store } from '@/redux/store';
 import { FC, PropsWithChildren } from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { persistor, store } from '@/redux/store';
 
 export const StoreProvider: FC<PropsWithChildren<{}>> = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { cartReducer } from '@/redux/features/cart';
 import { api } from '@/redux/services/api';
 
-export const store = configureStore({
-  reducer: {
+const persistConfig = {
+  key: 'root',
+  whitelist: ['cart'],
+  storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
     [api.reducerPath]: api.reducer,
     cart: cartReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([api.middleware]),
+  })
+);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat([api.middleware]),
   devTools: process.env.NODE_ENV !== 'production',
 });
+
+export const persistor = persistStore(store);
 
 export type StoreState = ReturnType<typeof store.getState>;
